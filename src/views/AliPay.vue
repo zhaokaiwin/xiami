0c44eb471b2c35ce684f9b144bf42a7601d58569
赵凯 <1296747577@qq.com>
2021-01-08 17:57:02 +0800

    支付宝支付功能

--- src/views/AliPay.vue
+++ src/views/AliPay.vue
@@ -1,19 +1,42 @@
 <template>
-  <div class="wrapper">ali-pay</div>
+  <div class="wrapper">
+    <div class="ali-pay">
+      <div class="form" v-html="content"></div>
+    </div>
+    <loading v-if="loading"></loading>
+  </div>
 </template>
 
 <script>
+import Loading from '@/components/Loading.vue'
 export default {
-  components: {},
+  components: { Loading },
   props: {},
-  data() {
-    return {}
+  data () {
+    return {
+      orderId: this.$route.query.orderId,
+      content: '',
+      loading: true
+    }
   },
-  watch: {},
-  computed: {},
-  methods: {},
-  created() {},
-  mounted() {}
+  methods: {
+    paySubmit () {
+      this.axios.post('/pay', {
+        orderId: this.orderId,
+        orderName: '凯哥哥',
+        amount: 0.01, // 单位元
+        payType: 1 // 支付类型
+      }).then(res => {
+        this.content = res.content
+        setTimeout(() => {
+          document.forms[0].submit()
+        }, 500)
+      })
+    }
+  },
+  mounted () {
+    this.paySubmit()
+  }
 }
 </script>
 <style lang="scss" scoped>
